92f5e833caaeadc357132e5e4a1de8272e40fafc
Mike Martino <mikemartino86@gmail.com>
2020-05-21 10:44:31 -0400

    add more tables and initialization data for recipes

--- database-initialization/1-schema.sql
+++ database-initialization/1-schema.sql
@@ -1,17 +1,31 @@
 CREATE SCHEMA recipebook;
 
--- Put new recipebook schema on the search path first.
-SET search_path TO recipebook, public;
-SHOW search_path;
-
-CREATE TABLE cook (
+CREATE TABLE recipebook.cook (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    email VARCHAR(200) NOT NULL
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL
 );
 
-INSERT INTO cook (first_name, last_name, email) VALUES
-  ('Mike', 'Martino', 'mikemartino86@gmail.com'),
-  ('Kate', 'Martino', 'kate.martin@live.ca');
+CREATE TABLE recipebook.recipe (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    cook_id INT REFERENCES recipebook.cook (id) ON DELETE RESTRICT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    cook_time INTERVAL,
+    instructions TEXT[] NOT NULL,
+    ingredients TEXT[] NOT NULL,
+    preface TEXT NOT NULL
+);
 
+CREATE TABLE recipebook.image (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    data BYTEA NOT NULL
+);
+
+CREATE TABLE recipebook.recipe_image (
+    recipe_id INT REFERENCES recipebook.recipe (id),
+    image_id INT REFERENCES recipebook.image (id),
+    PRIMARY KEY (recipe_id, image_id)
+)
